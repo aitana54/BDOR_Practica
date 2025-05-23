a9166e0f3b0fca4794380c0dfdc92cc169235ac4
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-05-23 21:26:58 +0200

    Ejercicio3 type & body

--- Archivo sql/Ejercicio3.sql
+++ Archivo sql/Ejercicio3.sql
@@ -7,4 +7,40 @@ CREATE OR REPLACE TYPE red_social_t AS OBJECT(
   cantidad_usuarios_millones NUMBER,
   ano_fundacion NUMBER,
   cotizacion_bolsa_millones NUMBER,
+
+  CONSTRUCTOR FUNCTION RED_SOCIAL_T(nombre VARCHAR2, usuarios NUMBER, ano NUMBER, cotizacion NUMBER) RETURN SELF AS RESULT,
+    MEMBER FUNCTION karma RETURN NUMBER,
+    MEMBER FUNCTION info_fundador RETURN VARCHAR2,
+    MAP MEMBER FUNCTION ordenar RETURN NUMBER
 );
+
+/
+
+CREATE OR REPLACE TYPE BODY RED_SOCIAL_T AS
+    CONSTRUCTOR FUNCTION RED_SOCIAL_T(nombre VARCHAR2, usuarios NUMBER, ano NUMBER, cotizacion NUMBER) RETURN SELF AS RESULT IS
+    BEGIN
+        SELF.nombre := nombre;
+        SELF.fundador := NULL;
+        SELF.pais := NULL;
+        SELF.cantidad_usuarios_millones := usuarios;
+        SELF.ano_fundacion := ano;
+        SELF.cotizacion := cotizacion;
+        RETURN;
+    END;
+
+    MEMBER FUNCTION karma RETURN NUMBER IS
+    BEGIN
+        RETURN (EXTRACT(YEAR FROM SYSDATE) - ano_fundacion) * (cantidad_usuarios_millones + cotizacion);
+    END;
+
+    MEMBER FUNCTION info_fundador RETURN VARCHAR2 IS
+    BEGIN
+        RETURN NVL(fundador, 'Dato Desconocido'); --En caso de que no el valor sea NULL
+    END;
+
+    MAP MEMBER FUNCTION ordenar RETURN NUMBER IS
+    BEGIN
+        RETURN cantidad_usuarios_millones;
+    END;
+END;
+/
